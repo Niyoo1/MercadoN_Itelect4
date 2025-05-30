268ebdd960a6bf3f21137fb0aa8281a3359cdfbd
francis.m.nabong@gmail.com <francis.m.nabong@gmail.com>
2025-05-30 14:02:21 +0800

    i edit employeeC

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -15,6 +15,26 @@ class employeecontroller extends Controller
         return view ('employee.index');
     }
 
+    public function index(){
+        employee::create($request->all());
+        return view ('employee.create');
+
+    }
+
+    public function index(Request $request){
+        employee::index($request ());
+        return view('Employee.create');
+        {
+
+            'fname = fname[255]string';
+            'lastname = lname[255]string';
+            'middlename = midname[255]string';
+            'age = age[255]integer';
+            'address= address[255]string';
+            'zip = zip[255]integer';
+        }
+
+    }
     // employee::create($request->all());
     // return view ('employee.create');
     // }
